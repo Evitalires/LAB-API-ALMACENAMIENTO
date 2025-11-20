7bcf3c7570676c4483b81e609079012ce14caea4
yudithleon <yudithkleon@gmail.com>
2025-11-20 13:01:10 -0500

    entrega tarea lista

--- scripts/saveFavorite.js
+++ scripts/saveFavorite.js
@@ -1,16 +1,11 @@
+import { MostrarPokemons } from "./mostrarPokemons.js"
 
+export const saveFavorite = pokemon => {
+  let arrayfavorito = JSON.parse(localStorage.getItem('favoritos')) || []
 
+  arrayfavorito.push(pokemon)
+  console.log(arrayfavorito)
+  localStorage.setItem('favoritos', JSON.stringify(arrayfavorito))
 
-export const saveFavorite = (pokemon)=>{
-
-let arrayfavorito=JSON.parse(localStorage.getItem('favoritos')) ||[];
-
-arrayfavorito.push(pokemon);
-console.log(arrayfavorito);
-localStorage.setItem('favoritos', JSON.stringify(arrayfavorito));
-
-
-
-  MostrarPokemons(favoritos);
-
+  MostrarPokemons(arrayfavorito)
 }
