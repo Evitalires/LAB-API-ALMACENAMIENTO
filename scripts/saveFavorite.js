3709557dcdcb60f1864a624e7d95e73480b4c6e1
romeDavidbenavides <benavidesrestrepodavid@gmail.com>
2025-11-20 11:40:02 -0500

    primer avanace

--- scripts/saveFavorite.js
+++ scripts/saveFavorite.js
@@ -1,5 +1,13 @@
+
+
+
 export const saveFavorite = (pokemon)=>{
 console.log("estoy en favorito", pokemon)
+let arrayfavorito=JSON.parse(localStorage.getItem('favoritos')) ||[];
+
+arrayfavorito.push(pokemon);
+console.log(arrayfavorito);
+localStorage.setItem('favoritos', JSON.stringify(arrayfavorito));
 
 //en un array strinfgFy
 // Guardarlo en el localstorage
