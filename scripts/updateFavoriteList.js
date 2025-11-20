7a05a6d6db2aa46271a49f4c01795d4cfc68f2f5
yudithleon <yudithkleon@gmail.com>
2025-11-20 11:11:43 -0500

    updatefavorite

--- scripts/updateFavoriteList.js
+++ scripts/updateFavoriteList.js
@@ -1,8 +1,20 @@
-export const updateFavorite = (pokemon)=>{
-console.log("estoy en favorito", pokemon)
-// traer con parse lo que tengo en el localstorge
- // filtro .find y busco el pokemons - nombre o id
- // cuando lo encuentre lo remueve del array y vuelve a el array completo al local estorage
+export const updateFavorite = ()=>{
+  let favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
+  const contenedor = document.getElementById("favoritos");
 
- //actualiza lo que se esta mostrando
+  contenedor.innerHTML = ""; 
+
+  favoritos.forEach(poke => {
+    const div = document.createElement("div");
+    div.classList.add("col-4", "col-md-2");
+
+    div.innerHTML = `
+      <div class="pokemon-card">
+        <img src="${poke.img}" width="100">
+        <p class="mt-2">${poke.name}</p>
+      </div>
+    `;
+
+    contenedor.appendChild(div);
+  });
 }
