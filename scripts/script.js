709d88d96712b030c31f8a84d4f606702ddeb95c
yudithleon <yudithkleon@gmail.com>
2025-11-20 13:12:50 -0500

    tarea entregada

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,14 @@
 import { MostrarPokemons } from "./mostrarPokemons.js";
 import { saveFavorite } from "./saveFavorite.js";
 import { searchPokemon } from "./searchPokemon.js";
+import { updateFavorite } from "./updateFavoriteList.js";
 
 const buscarBtn = document.getElementById("buscarBtn");
 const pokemonInput = document.getElementById("pokemonInput");
 const pokemonCard = document.getElementById("pokemonCard");
 const botonFavoritos = document.getElementById("favoritos");
+
+const botonLimpiar = document.getElementById("limpiar");
 
 document.addEventListener("DOMContentLoaded", async function () {
   let favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
@@ -28,4 +31,8 @@ buscarBtn.addEventListener("click", () => {
 botonFavoritos.addEventListener("click", () => {
   let currentPoke = JSON.parse(localStorage.getItem("currentPokemon"));
   saveFavorite(currentPoke);
+});
+
+botonLimpiar.addEventListener("click", () => {
+  updateFavorite()
 });
--- scripts/updateFavoriteList.js
+++ scripts/updateFavoriteList.js
@@ -1,20 +1,8 @@
+import { MostrarPokemons } from "./mostrarPokemons.js";
+
 export const updateFavorite = ()=>{
-  let favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
-  const contenedor = document.getElementById("favoritosMostrar");
+localStorage.removeItem("favoritos");
 
-  contenedor.innerHTML = ""; 
-
-  favoritos.forEach(poke => {
-    const div = document.createElement("div");
-    div.classList.add("col-4", "col-md-2");
-
-    div.innerHTML = `
-      <div class="pokemon-card">
-        <img src="${poke.img}" width="100">
-        <p class="mt-2">${poke.name}</p>
-      </div>
-    `;
-
-    contenedor.appendChild(div);
-  });
+MostrarPokemons()
+ 
 }
